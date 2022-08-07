3691541cb2b4a9c9291cac216b3093cd6a2a808d
Teddy Cubaka <teddycubaka96@gmail.com>
2022-08-07 15:06:52 +0100

    first commit in the branch main

---
+++ src/App.js
@@ -0,0 +1,24 @@
+// import './App.css';
+import About from './components/about.js';
+import Contact from './components/contact.js';
+import Home from './components/home'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
+
+
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+          Fait gaff
+      </div>
+  </Router>
+  );
+}
+
+export default App;
